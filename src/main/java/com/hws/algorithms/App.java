553447d63dee8816db693278a7c841935c9cfab7
ArcWarden <hongweisheng77@gmail.com>
2025-09-08 00:53:08 -0400

    Initial Java project with Maven

---
+++ src/main/java/com/hws/algorithms/App.java
@@ -0,0 +1,13 @@
+package com.hws.algorithms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
